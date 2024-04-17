0251c639326fe06180f3fb6151dc21b30e62a129
Maria Clara <161609445+mariaoliveira27@users.noreply.github.com>
2024-04-17 12:09:06 -0300

    Add files via upload

---
+++ provaAV1/src/provaAV1.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : provaAV1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int jogoPorta(){
+	int portaEscolhida, portaPremiada, portaVazia;
+	char opcao;
+
+	printf("Bem vindo ao jogo Monty Hall!");
+	printf("Escolha uma porta(1 a 3):");
+	scanf("%d",&portaEscolhida);
+
+	portaPremiada= srand()%3+1
+}
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
