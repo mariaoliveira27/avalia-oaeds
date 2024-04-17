54f1b362a2822334956392bc0f65a1b4f24a6348
Maria Clara <161609445+mariaoliveira27@users.noreply.github.com>
2024-04-17 12:14:57 -0300

    Update provaAV1.c

--- provaAV1/src/provaAV1.c
+++ provaAV1/src/provaAV1.c
@@ -1,56 +1,62 @@
+/*
+ ============================================================================
+ Name        : provaaeds1_I.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(void) {
-  int portaPremiada, portaEscolhida, portaVazia, i;
-  char troca;
-  printf("Bem vindo ao jogo do monty hall!\n");
-  printf("Escolha uma porta entre 1 e 3: ");
-  scanf("%d", &portaEscolhida);
+	int portaPremiada, portaEscolhida, portaVazia, i;
+	char troca;
+	printf("Bem vindo ao jogo do monty hall!\n");
+	printf("Escolha uma porta entre 1 e 3: ");
+	scanf("%d", &portaEscolhida);
 
-  srand(time(NULL));
-  portaPremiada = rand() % 3 + 1;
+	srand(time(NULL));
+	portaPremiada = rand() % 3 + 1;
 
+	do {
+		portaVazia = rand() % 3 + 1;
 
-  do{
-    portaVazia = rand() % 3 + 1;
+	} while (i < 3 + 1);
 
-  }while (i < 3+1);
+	{
+		if (i != portaPremiada && i != portaEscolhida) {
+			i++;
+		} else {
+			portaVazia = i;
+			i++;
+		}
+	}
 
+	printf("Vamos abrir a porta %d!", portaVazia);
+	printf("\nA porta %d não tem o prêmio\n", portaVazia);
 
+	printf("Deseja trocar de porta? (s/n): ");
+	scanf(" %c", &troca);
 
-  {
-  if (i != portaPremiada && i != portaEscolhida){
-      i++;
-    }else{
-     portaVazia = i;
-      i++;
-    }
-  }
+	if (troca == 's') {
+		printf(
+				"Digite um numero diferente da porta escolhida inicialmente(entre 1 "
+						"e 3):");
+		scanf("%d", &portaEscolhida);
+	}
+	if (troca == 'n') {
+		printf("Você não trocou de porta!");
+	}
 
-
-  printf("Vamos abrir a porta %d!", portaVazia);
-  printf("\nA porta %d não tem o prêmio\n", portaVazia);
-
-  printf("Deseja trocar de porta? (s/n): ");
-  scanf(" %c", &troca);
-
-  if (troca == 's') {
-    printf("Digite um numero diferente da porta escolhida inicialmente(entre 1 "
-           "e 3):");
-    scanf("%d", &portaEscolhida);
-  }
-  if(troca == 'n'){
-    printf("Você não trocou de porta!");
-  }
-
-  if (portaEscolhida == portaPremiada) {
-    printf("\nParabéns você venceu!");
-  } else {
-    printf("\nVocê perdeu!");
-  }
-  printf("\nA porta premiada é %d!", portaPremiada);
-  return 0;
+	if (portaEscolhida == portaPremiada) {
+		printf("\nParabéns você venceu!");
+	} else {
+		printf("\nVocê perdeu!");
+	}
+	printf("\nA porta premiada é %d!", portaPremiada);
+	return 0;
 }
-
